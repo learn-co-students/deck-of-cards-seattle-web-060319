8527f90f6907ced0079a42d92818158d0553802a
Claire Wilbert <claire.wilbert@gmail.com>
2019-06-17 09:46:56 -0700

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,31 @@
-class Deck
+#feathers
 
+require'pry'
+
+class Deck
+ attr_accessor :cards
+ def initialize
+   @cards = []
+
+   for suit in ["Hearts", "Clubs", "Diamonds", "Spades"] do
+     for rank in ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"] do
+     @cards << Card.new(suit, rank)
+     end
+   end
+ end
+
+ def choose_card
+   cards.delete_at(rand(cards.length))
+ end
 end
 
 class Card
-
+ attr_accessor :rank, :suit
+ def initialize(suit, rank)
+   @rank = rank
+   @suit = suit
+ end
 end
+
+binding.pry
+0
